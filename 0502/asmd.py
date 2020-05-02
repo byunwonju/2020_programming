a58c14a26cad3dc5e528e3f2c1245d6ed6f2f1cb
byunwonju <63887978+byunwonju@users.noreply.github.com>
2020-05-02 12:41:18 +0900

    Add files via upload

---
+++ 0502/asmd.py
@@ -0,0 +1,8 @@
+# 두 수를 입력받아 사칙연산 결과를 출력하는 프로그램
+num1=int(input("첫 번째 정수 입력:"))
+num2=int(input("두 번째 정수 입력:"))
+
+print("{:2d} +{:2d}={:3d}".format(num1, num2,(num1 + num2))
+print("{:2d} -{:2d}={:3d}".format(num1, num2,(num1 - num2))
+print("{:2d} *{:2d}={:3d}".format(num1, num2,(num1 * num2))
+print("{:2d} /{:2d}={:3d}".format(num1, num2,(num1 / num2))
